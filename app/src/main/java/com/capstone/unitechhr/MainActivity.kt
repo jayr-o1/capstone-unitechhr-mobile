4eddf92c79a0197f4823850cff29eddaf57ff7cd
jayrmalazarte.olores <jayrmalazarte.olores@gmail.com>
2025-04-18 05:16:53 +0000

    changes

--- app/src/main/java/com/capstone/unitechhr/MainActivity.kt
+++ app/src/main/java/com/capstone/unitechhr/MainActivity.kt
@@ -4,17 +4,35 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.capstone.unitechhr.fragments.ForgotPasswordFragment
+import com.capstone.unitechhr.fragments.LoginFragment
+import com.capstone.unitechhr.fragments.RegistrationFragment
+import com.capstone.unitechhr.fragments.ResetPasswordFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        goToFragment(LoginFragment())
+    }
+
+    fun goToFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        when (fragment) {
+            is LoginFragment -> {
+                transaction.replace(R.id.fragment_container, fragment)
+            }
+            is RegistrationFragment -> {
+                transaction.replace(R.id.fragment_container, fragment)
+            }
+            is ForgotPasswordFragment -> transaction.replace(R.id.fragment_container, fragment)
+            is ResetPasswordFragment -> transaction.replace(R.id.fragment_container, fragment)
         }
+
+        transaction.commit()
     }
 }
