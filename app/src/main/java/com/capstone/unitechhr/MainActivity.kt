8646bc1ca69f9e33af9554ca6b49a8d2421e1d26
Jay-r Olores <jayrmalazarte.olores@gmail.com>
2025-04-19 12:11:25 +0800

    Add toolbar to MainActivity and update layout constraints

--- app/src/main/java/com/capstone/unitechhr/MainActivity.kt
+++ app/src/main/java/com/capstone/unitechhr/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     
@@ -22,6 +23,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        
+        // Set up the toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
