7702cbfdd7c2c052d1ac9d90d754a92ca8565f54
Jay-r Olores <jayrmalazarte.olores@gmail.com>
2025-04-19 12:15:00 +0800

    Refactor MainActivity by removing the toolbar and associated action bar setup, and update layout constraints in activity_main.xml for improved UI structure.

--- app/src/main/java/com/capstone/unitechhr/MainActivity.kt
+++ app/src/main/java/com/capstone/unitechhr/MainActivity.kt
@@ -7,26 +7,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     
     private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
-        // Set up the toolbar
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
         
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -42,21 +33,5 @@ class MainActivity : AppCompatActivity() {
         // Setup Bottom Navigation
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.setupWithNavController(navController)
-        
-        // Setup Action Bar with Navigation
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.jobListingFragment,
-                R.id.interviewScheduleFragment,
-                R.id.applicantListFragment,
-                R.id.profileFragment
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-    }
-    
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
